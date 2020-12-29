575e46e57d9b08466829977794d2fe4105f5b68e
Theo5607 <theo67.jeu@gmail.com>
2020-12-29 15:12:15 +0100

    yolo

--- casino.py
+++ casino.py
@@ -1,24 +1,63 @@
+import sys, time, pygame
 from tkinter import *
 from sqlite3 import *
-import sys, time, pygame
+from pygame.locals import *
+from Casino_Matvei.test import slot_partie
 
 conn = connect("data.txt")
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS membres (identifiant TEXT, mdp TEXT, argent INTEGER)")
 
 black = 0, 0, 0
+white = 255, 255, 255
 jouer=1
+tableau='entree'
 
 pygame.init()
 
 screen = pygame.display.set_mode((1600, 900))
 
-entreecasino = pygame.image.load("Images/Menu/entree_casino.png")
-
 connexion = pygame.image.load("Images/connexion.png")
 
+#Images entree casino
+
+entreecasino = pygame.image.load("Images/Menu/entree_casino.png")
 btn_entrer = pygame.image.load("Images/Menu/btn_entrer.png")
 btn_entrer_pos = (650, 700)
+
+#Images menu choix du jeu
+
+banderole = pygame.image.load("Images/Menu_jeu/banderole.png")
+blackjack_icone = pygame.image.load("Images/Menu_jeu/blackjack_icone.png")
+blackjack_icone_pos = (250, 170)
+slot_icone = pygame.image.load("Images/Menu_jeu/slot_icone.png")
+slot_icone_pos = (900, 170)
+roulette_icone = pygame.image.load("Images/Menu_jeu/roulette_icone.png")
+roulette_icone_pos = (500, 550)
+
+#-----------
+
+#Images machine à sous
+
+mas_jouer = pygame.image.load("Images/Jeux/Machine_a_sous/jouer.png")
+mas_jouer_pos = (300, 300)
+mas_retour = pygame.image.load("Images/Jeux/Machine_a_sous/retour.png")
+mas_retour_pos = (900, 300)
+
+mas_somme = pygame.image.load("Images/Jeux/Machine_a_sous/texte_somme.png")
+mas_rectangle_somme = pygame.image.load("Images/Jeux/Machine_a_sous/rectangle_somme.png")
+mas_rectangle_somme_pos = (500, 600)
+
+mas_gains = pygame.image.load("Images/Jeux/Machine_a_sous/gains.png")
+mas_gains_pos = (500, 400)
+
+mas_somme_non_valide = pygame.image.load("Images/Jeux/Machine_a_sous/somme_non_valide.png")
+
+mas_rejouer = pygame.image.load("Images/Jeux/Machine_a_sous/rejouer.png")
+mas_rejouer_pos = (550, 700)
+mas_quitter = pygame.image.load("Images/Jeux/Machine_a_sous/quitter.png")
+mas_quitter_pos = (1050, 700)
+
 
 screen.blit(connexion, (0, 0))
 pygame.display.flip()
@@ -59,6 +98,13 @@ def crea_liste_cartes(): #Renvoie un dictionnaire qui renvoie pour une clé qui 
 
 liste_cartes=crea_liste_cartes()
 
+liste_nb = (K_0, K_1, K_2, K_3, K_4, K_5, K_6, K_7, K_8, K_9)
+dico_nb = {}
+var=0
+for el in liste_nb:
+    dico_nb[el]=str(var)
+    var+=1
+
 
 def go(): #Pour se connecter au casino et lancer le programme
     print('Bienvenue au Casino ! Veuillez commencer par créer un compte ou vous connecter.')
@@ -87,10 +133,24 @@ def go(): #Pour se connecter au casino et lancer le programme
         compte=[id_entre, mdp_entre, argent_sing[0]]
             
         print('Bienvenue, '+id_entre)
-                
+
+        screen.fill(black)
+        screen.blit(entreecasino, (0, 0))
+        screen.blit(btn_entrer, btn_entrer_pos)
+        pygame.display.flip()
+
+        return compte
+
     if var=='1':
         compte=creation_compte()
         print('Bienvenue, '+compte[0])
+                
+        screen.fill(black)
+        screen.blit(entreecasino, (0, 0))
+        screen.blit(btn_entrer, btn_entrer_pos)
+        pygame.display.flip()
+
+        return compte
 
     #hall(compte)
 
@@ -150,10 +210,159 @@ def hall(compte): #(SERA PROBABLEMENT BIENTOT OBSOLETE) Fonction "menu" qui perm
     if jeu==0:
         return
 
-go()
+compte=go()
 
+#variables machine à sous
+font = pygame.font.Font("Polices/dejavu_sansbold.ttf", 100)
+font2 = pygame.font.Font("Polices/dejavu_sansbold.ttf", 50)
+somme=''
+gains=0
+        
 while jouer==1:
-    screen.fill(black)
-    screen.blit(entreecasino, (0, 0))
-    screen.blit(btn_entrer, btn_entrer_pos)
-    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            jouer = 0
+            
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            #bouton entree
+            if(event.pos[0]>=650 and event.pos[0]<=950) and (event.pos[1]>=700 and event.pos[1]<=800) and tableau=='entree':
+                tableau='menu'
+
+                screen.fill(white)
+                screen.blit(banderole, (0, 0))
+                screen.blit(blackjack_icone, blackjack_icone_pos)
+                screen.blit(slot_icone, slot_icone_pos)
+                screen.blit(roulette_icone, roulette_icone_pos)
+                pygame.display.flip()
+
+            #boutons menu jeu
+            if(event.pos[0]>=900 and event.pos[0]<=1350) and (event.pos[1]>=170 and event.pos[1]<=420) and tableau=='menu':
+                tableau='mas_menu'
+                
+                screen.fill(white)
+                screen.blit(mas_jouer, mas_jouer_pos)
+                screen.blit(mas_retour, mas_retour_pos)
+                pygame.display.flip()
+
+            #------------
+
+            #boutons menu machine à sous
+            if(event.pos[0]>=300 and event.pos[0]<=700) and (event.pos[1]>=300 and event.pos[1]<=500) and tableau=='mas_menu':
+                tableau='mas_somme'
+
+                screen.fill(white)
+                screen.blit(mas_somme, (0, 0))
+                screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                pygame.display.flip()
+                
+            if(event.pos[0]>=900 and event.pos[0]<=1300) and (event.pos[1]>=300 and event.pos[1]<=500) and tableau=='mas_menu':
+                tableau='menu'
+
+                screen.fill(white)
+                screen.blit(banderole, (0, 0))
+                screen.blit(blackjack_icone, blackjack_icone_pos)
+                screen.blit(slot_icone, slot_icone_pos)
+                screen.blit(roulette_icone, roulette_icone_pos)
+                pygame.display.flip()
+
+            #boutons rectangle somme machine à sous
+            if(event.pos[0]>=600 and event.pos[0]<=1000) and (event.pos[1]>=600 and event.pos[1]<=700) and tableau=='mas_somme':
+                somme=''
+                tableau='mas_entrer_somme'
+                
+                afficher = font.render('0', 1, (0, 0, 0))
+                screen.blit(afficher, (500,600))
+                pygame.display.flip()
+
+            #boutons quitter et rejouer machine à sous
+            if(event.pos[0]>=550 and event.pos[0]<=850) and (event.pos[1]>=700 and event.pos[1]<=800) and tableau=='mas_gains':
+                tableau='mas_somme'
+
+                screen.fill(white)
+                screen.blit(mas_somme, (0, 0))
+                screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                pygame.display.flip()
+
+            if(event.pos[0]>=1050 and event.pos[0]<=1350) and (event.pos[1]>=700 and event.pos[1]<=800) and tableau=='mas_gains':
+                tableau='menu'
+
+                screen.fill(white)
+                screen.blit(banderole, (0, 0))
+                screen.blit(blackjack_icone, blackjack_icone_pos)
+                screen.blit(slot_icone, slot_icone_pos)
+                screen.blit(roulette_icone, roulette_icone_pos)
+                pygame.display.flip()
+
+        #jouer à la machine à sous
+        if event.type == KEYDOWN and tableau=='mas_entrer_somme':
+            for el in liste_nb:
+                if el==event.key:
+                    if len(somme)<8:
+                        screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                    
+                        somme=somme+dico_nb[el]
+                        afficher = font.render(somme, 1, (0, 0, 0))
+                        screen.blit(afficher, (500,600))
+                        pygame.display.flip()
+                    else:
+                        somme=somme[0:7]+dico_nb[el]
+
+                        screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                        
+                        afficher = font.render(somme, 1, (0, 0, 0))
+                        screen.blit(afficher, (500,600))
+                        pygame.display.flip()
+
+            if event.key == K_BACKSPACE:
+                screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                
+                somme=somme[0:len(somme)-1]
+                afficher = font.render(somme, 1, (0, 0, 0))
+                screen.blit(afficher, (500,600))
+                pygame.display.flip()
+
+            if event.key == K_RETURN:
+                if somme=='':
+                    somme='0'
+                if compte[2]>=int(somme) and int(somme)>0:
+                    tableau='mas_gains'
+
+                    gains=slot_partie(int(somme))
+                    
+                    screen.fill(white)
+
+                    if gains > 0 :
+                        compte[2]=compte[2]-int(somme)+gains
+                        
+                        cur.execute("UPDATE membres SET argent = ? WHERE identifiant = ?", (compte[2], compte[0]))
+                        conn.commit()
+                        
+                        screen.blit(mas_gains, mas_gains_pos)
+                        afficher = font2.render('Vous avez gagné '+str(gains)+' dollars', 1, (0, 0, 0))
+                        screen.blit(afficher, (500,400))
+                        screen.blit(mas_rejouer, mas_rejouer_pos)
+                        screen.blit(mas_quitter, mas_quitter_pos)
+                        pygame.display.flip()
+                    else:
+                        compte[2]=compte[2]-int(somme)
+
+                        cur.execute("UPDATE membres SET argent = ? WHERE identifiant = ?", (compte[2], compte[0]))
+                        conn.commit()
+
+                        screen.blit(mas_gains, mas_gains_pos)
+                        afficher = font2.render('Vous avez perdu votre mise', 1, (0, 0, 0))
+                        screen.blit(afficher, (500,400))
+                        screen.blit(mas_rejouer, mas_rejouer_pos)
+                        screen.blit(mas_quitter, mas_quitter_pos)
+                        pygame.display.flip()
+                else:
+                    tableau='mas_somme'
+
+                    screen.fill(white)
+                    screen.blit(mas_somme_non_valide, (0, 0))
+                    screen.blit(mas_rectangle_somme, mas_rectangle_somme_pos)
+                    pygame.display.flip()
+
+                
+
+            
