b03698dc1c14fb2a62507e65415b811da6482d4f
Theo5607 <53973847+Theo5607@users.noreply.github.com>
2021-01-06 11:42:11 +0100

    Update blackjack.py

--- Casino_Teis/blackjack.py
+++ Casino_Teis/blackjack.py
@@ -1,65 +1,51 @@
-from random import *
+import random
 
-def blackjack():
-    nb_j = 0
-    somme_joueurs = {}
-    somme_croupier = 0
-
-    while nb_j < 2:
-        nb_j = int(input('Entrez un nombre de joueurs: '))
-    for i in range(1,nb_j+1):
-        somme = 0
-        carte1 = randint(1,13)
-        carte2 = randint(1,13)
-        carte_croupier = randint(1,13)
-        print('Le croupier tire les cartes du joueurs',i,'...')
-        if carte1 == 1:
-            print('La première carte est un as')
-            somme = 11
-        elif carte1 == 11:
-            print('La première carte est un valet')
-            somme = 10
-        elif carte1 == 12:
-            print('La première carte est une dame')
-            somme = 10
-        elif carte1 == 13:
-            print('La première carte est un roi')
-            somme = 10
-        else:
-            print('La première carte est un',carte1)
-            somme = carte1
-        if carte2 == 1:
-            print('La deuxième carte est un as')
-            somme = somme+11
-        elif carte2 == 11:
-            print('La deuxième carte est un valet')
-            somme = somme+10
-        elif carte2 == 12:
-            print('La deuxième carte est une dame')
-            somme = somme+10
-        elif carte2 == 13:
-            print('La deuxième carte est un roi')
-            somme = somme+10
-        else:
-            print('La deuxième carte est un',carte2)
-            somme = somme+carte2
-        print('Vous êtes à',somme)
-        somme_joueurs[i] = somme
-    print('Le croupier tire sa carte')
-    if carte_croupier == 1:
-        print('La carte du croupier est un as')
-        somme_croupier = somme_croupier+11
-    elif carte_croupier == 11:
-        print('La carte du croupier est un valet')
-        somme_croupier = somme_croupier+10
-    elif carte_croupier == 12:
-        print('La carte du croupier est une dame')
-        somme_croupier = somme_croupier+10
-    elif carte_croupier == 13:
-        print('La carte du croupier est un roi')
-        somme_croupier = somme_croupier+10
+def ret_spec(nb): #Retourne le nom de la carte
+    if nb==11:
+        return 'valet'
+    elif nb==12:
+        return 'dame'
+    elif nb==13:
+        return 'roi'
+    elif nb==1:
+        return 'as'
     else:
-        print('La carte du croupier est un',carte_croupier)
-        somme_croupier = somme_croupier+carte2
-    print('___________')
-    print('La somme du croupier est',somme_croupier)
+        return str(nb)
+
+def crea_liste_cartes(): #Renvoie un dictionnaire qui renvoie pour une clé qui va de 1 à 52 pour le numéro cartes une liste qui contient la valeur de la carte, son nom et sa couleur
+    liste_cartes={}
+
+    for i in range(1,53):
+        nb=i
+        famille_nb=0
+
+        while nb > 13:
+            nb=nb-13
+            famille_nb=famille_nb+1
+
+        if famille_nb==0:
+            liste_cartes[i]=[nb,ret_spec(nb),'coeur']
+        elif famille_nb==1:
+            liste_cartes[i]=[nb,ret_spec(nb),'carreau']
+        elif famille_nb==2:
+            liste_cartes[i]=[nb,ret_spec(nb),'pique']
+        elif famille_nb==3:
+            liste_cartes[i]=[nb,ret_spec(nb),'trèfle']
+    
+    return liste_cartes
+    
+liste_cartes=crea_liste_cartes()
+    
+    
+def tirer_carte(cartes):
+    nb_carte = random.randint(1,54)
+        
+    return(liste_cartes[nb_carte])
+    
+
+def blackjack(mise, somme_cartes_croupier, somme_cartes_joueur):
+    gains = 0
+    if somme_cartes_joueur >= somme_cartes_croupier:
+        gains = mise*2
+    
+    return gains
