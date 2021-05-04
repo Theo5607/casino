e2c824bd62cbb6046e351cb62e2ac5e107b74185
MatveiBG <70953270+MatveiBG@users.noreply.github.com>
2021-05-04 15:34:36 +0200

    rename symboles dans le visuel

--- Casino_Matvei/animation_slot.py
+++ Casino_Matvei/animation_slot.py
@@ -13,15 +13,12 @@ def animation_machine(liste_de_symboles):
 #importation de toutes les images
 
     bar= pygame.image.load("images/bar.png")
-    bar_2= pygame.image.load("images/bar_2.png")
-    bar_3= pygame.image.load("images/bar_3.png")
+    watermelon= pygame.image.load("images/watermelon.png")
+    cherry= pygame.image.load("images/cherry.png")
     symb_7= pygame.image.load("images/symb_7.png")
-    bar= pygame.image.load("images/bar.png")
-    scatter= pygame.image.load("images/scatter.png")
+    diamond= pygame.image.load("images/diamond.png")
     cloche=pygame.image.load("images/cloche.png")
-    lemon = pygame.image.load("images/lemon_temp.png")
-    
-    #defintion des postions possibles pour les symboles, allant de gauche a droite et du haut vers le bas
+    lemon = pygame.image.load("images/lemon.png")
 
     pos_1=(495, 95)
     pos_2=(700, 95)
@@ -34,9 +31,10 @@ def animation_machine(liste_de_symboles):
     pos_9=(905, 505)
     list_pos=[pos_1,pos_2,pos_3,pos_4,pos_5,pos_6,pos_7,pos_8,pos_9]
 
+    #symboles=['7','cloche','cherry','watermelon','bar','lemon','scatter']
+
     list_symbole = slot_partie(13)[0]
-    
-    #recupere les positions pour les symboles sortant de slot_partie
+
 
     for i in range (9):
         if list_symbole[i-1]==0:
